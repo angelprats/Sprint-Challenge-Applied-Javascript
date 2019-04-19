4cb8d67692e9e9a45477bfbe8a46cb4594b38c0e
angel prats <angelpratz@gmail.com>
2019-04-19 18:15:52 -0400

    fixed all bugs added extra content

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -11,11 +11,11 @@ class TabLink {
     // Check to see if this.tabData is equal to 'all'
     if (this.tabData === 'all') {
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelector('.card');
+      this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       this.cards = document.querySelectorAll(
-        `.cards[data-tab='${this.tabData}']`
+        `.card[data-tab='${this.tabData}']`
       );
     }
 
